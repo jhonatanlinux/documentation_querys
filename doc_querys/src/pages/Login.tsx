53a6d329c2673d7c2b2280926f7b07cefe968792
jhonatanlinux <jhonatan135135@gmail.com>
2023-10-31 17:43:57 -0400

    seção 26 - ok

--- doc_querys/src/pages/Login.tsx
+++ doc_querys/src/pages/Login.tsx
@@ -1,25 +1,121 @@
-import loginImg from "../assets/topsapp_icon.png";
+import React, { useState } from "react";
+import imgTopSapp from "../assets/login_03.png";
+import bgVideo from "../assets/video_bg.mp4";
 
 function Login() {
+  const [displayLogin, setDisplayLogin] = useState(true);
+  const [displaySignUp, setDisplaySignUp] = useState(false);
+
+  const handleDisplayCreateAccount = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    setDisplayLogin(false);
+    setDisplaySignUp(true);
+  };
+
+  const handleDisplayLogin = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    setDisplayLogin(true);
+    setDisplaySignUp(false);
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
-      <div className="flex flex-col justify-center">
-        <div className="flex flex-col justify-center align-itens-center mb-14">
-          <h1 className="text-2xl text-gray-100 text-center font-semibold">
-            Documentations Querys 💻
-          </h1>
-          <form className="max-h-[400px] w-full mx-auto bg-gray-100 p-8 rounded-lg">
-            <h2 className="text-2xl dark:text-white font-bold text-center">Login</h2>
-          </form>
+    <div className="relative flex justify-center items-center h-screen overflow-hidden px-8 py-12">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute inset-0 object-cover w-full h-full z-0 filter blur-sm"
+      >
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      {displayLogin && (
+        <div className="relative flex flex-col items-center bg-blue-500 p-8 rounded-lg shadow-lg min-w-[400px] z-10">
+          <div
+            className="bg-cover bg-center h-20 w-60 mb-8"
+            style={{
+              backgroundImage: `url(${imgTopSapp})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <h2 className="text-2xl text-white mb-4">
+            Faça o login em sua conta!
+          </h2>
+          <div className="flex flex-col w-full text-white mb-4">
+            <label className="mb-1">Email</label>
+            <input
+              type="email"
+              className="rounded-lg mt-2 p-2 border text-gray-900"
+              placeholder="Digite seu email!"
+            />
+          </div>
+          <div className="flex flex-col w-full text-white mb-4">
+            <label className="mb-1">Senha</label>
+            <input
+              type="password"
+              className="rounded-lg mt-2 p-2 border text-gray-900"
+              placeholder="Digite sua senha!"
+              maxLength={10}
+              minLength={4}
+            />
+          </div>
+          <div className="px-4 py-2 mb-1">
+            <button
+              type="submit"
+              className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 font-bold hover:cursor-pointer transition duration-300 shadow-lg enabled:hover:shadow-blue-900 disabled:bg-blue-200 disabled:shadow-none enabled:shadow-200/50"
+            >
+              Entrar
+            </button>
+          </div>
+          <div className="px-4 py-2 mb-1">
+            <button
+              type="button"
+              onClick={(event) => handleDisplayCreateAccount(event)}
+              className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 font-bold hover:cursor-pointer transition duration-300 hover:animate-pulse shadow-lg enabled:hover:shadow-blue-900 disabled:bg-blue-200 disabled:shadow-none enabled:shadow-200/50"
+            >
+              Criar Conta
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="sm:flex hidden justify-center items-center">
-        <img
-          className="w-full h-screen object-cover"
-          src={loginImg}
-          alt="TopSapp"
-        />
-      </div>
+      )}
+
+      {displaySignUp && (
+        <div className="relative flex flex-col items-center bg-blue-500 p-8 rounded-lg shadow-lg min-w-[400px] z-10">
+          <div
+            className="bg-cover bg-center h-20 w-60 mb-8"
+            style={{
+              backgroundImage: `url(${imgTopSapp})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <h2 className="text-2xl text-white mb-4">Criar Conta</h2>
+          <div className="flex flex-col w-full text-white mb-4">
+            <label className="mb-1">Email</label>
+            <input
+              type="email"
+              className="rounded-lg mt-2 p-2 border text-gray-900"
+              placeholder="Cadastre seu email!"
+            />
+          </div>
+          <div className="flex flex-col w-full text-white mb-4">
+            <label className="mb-1">Senha</label>
+            <input
+              type="password"
+              className="rounded-lg mt-2 p-2 border text-gray-900"
+              placeholder="Cadastre sua senha!"
+              maxLength={10}
+              minLength={4}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
